e2877c9ff30f3d25282f56b1ab1aa604ce5115e9
Matt Cotter <mcotter222@gmail.com>
2014-08-14 13:37:39 -0400

    add aggregation example in demo

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ mongod --dbpath ./data/db/ --logpath ./data/log --fork
 # note, you must fist get the taxi data from:
 # http://www.andresmh.com/nyctaxitrips/
 python loader.py
+mongo localhost:27017/taxi --eval "printjson(db.fares.ensureIndex({pickup_time:1}))"
+mongo localhost:27017/taxi --eval "printjson(db.trips.ensureIndex({distance:1}))"
