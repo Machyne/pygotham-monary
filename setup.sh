2ff99d3692270940f5c077f45a858dcc53a28524
Matt Cotter <mcotter222@gmail.com>
2014-08-14 11:02:58 -0400

    Clean up -- remove old files

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 killall mongod
 rm -rf ./data
 mkdir -p ./data/db
-mongod --dbpath ./data/db/ --logpath ./data/log --auth --fork
+mongod --dbpath ./data/db/ --logpath ./data/log --fork
 # note, you must fist get the taxi data from:
 # http://www.andresmh.com/nyctaxitrips/
 python loader.py
