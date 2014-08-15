6372bd58d8401ed0de9cecac032dd5d342a4b7b1
Matt Cotter <mcotter222@gmail.com>
2014-08-15 14:35:30 -0400

    Change comment in .sh to `if` and `echo`

--- setup.sh
+++ setup.sh
@@ -4,10 +4,15 @@ killall mongod
 rm -rf ./data
 mkdir -p ./data/db
 mongod --dbpath ./data/db/ --logpath ./data/log --fork
-# note, you must fist get the taxi data from:
-# http://www.andresmh.com/nyctaxitrips/
-# We downloaded 1 file from each trip_data and trip_fares and took the first
-# 3,000,001 lines from each (3million records + the header)
+
+if [[ ! -f "./taxi/trip_data_small.csv" || ! -f "./taxi/trip_fare_small.csv"]]; then
+    echo "note, you must fist get the taxi data from:"
+    echo "http://www.andresmh.com/nyctaxitrips/"
+    echo "We downloaded 1 file from each trip_data and trip_fares and took the first"
+    echo "3,000,001 lines from each (3million records + the header)"
+    exit 1
+fi
+
 python loader.py
 mongo localhost:27017/taxi --eval "printjson(db.fares.ensureIndex({pickup_time:1}))"
 mongo localhost:27017/taxi --eval "printjson(db.trips.ensureIndex({distance:1}))"
